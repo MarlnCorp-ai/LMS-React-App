afddea7cc8870ebae289d272e380a66851cde979
BADRI NARAYANAN <badhrirajen@gmail.com>
2025-03-23 13:55:00 -0400

    Installed some packages, modified project structure, Ignored some files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./Pages/LandingPage";
+import IndividualPricing from "./Pages/IndividualPricing";
+import MainLayout from "./Pages/MainLayout";
+import ErrorPage from "./Pages/ErrorPage";
+import BusinessPricing from "./Pages/BusinessPricing";
+import PublicSectorPricing from "./Pages/PublicSectorPricing";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <LandingPage /> },
+      { path: "/individuals/pricing", element: <IndividualPricing /> },
+      {path:"/businesses/pricing", element: <BusinessPricing />},
+      {path:"/industries/public-sector/pricing", element: <PublicSectorPricing />}
+    ],
+  },
+]);
+
 function App() {
-  return <h1>This is a LMS Application!</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
