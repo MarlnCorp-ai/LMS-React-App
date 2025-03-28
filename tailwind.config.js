d1957bc6491c624b81caa12f43144a1dd7d2eb61
BADRI NARAYANAN <badhrirajen@gmail.com>
2025-03-28 02:04:04 -0400

    Modified tailwindcss version to 3.x

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ["./public/index.html", "./src/**/*.{js,jsx}"],
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
